7c31214790e747ebc5c1b27d9f91047b2df81b3f
Icaro Asas Virtuais <39817766+asasvirtuais@users.noreply.github.com>
2025-10-07 12:07:51 +0000

    removed initial files and sketched some UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,35 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Button, Card, Container, Heading, Stack } from '@chakra-ui/react'
+import Link from 'next/link'
+
+const surveys = [
+  { title: 'Survey One' },
+  { title: 'Survey Two' },
+  { title: 'Survey Three' },
+  { title: 'Survey Four' },
+]
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  return (
+    <Container maxW='breakpoint-sm'>
+      <Stack py={4} gap={4}>
+        <Heading>Survey App</Heading>
+        {surveys.map(survey => (
+          <Card.Root>
+            <Card.Body>
+              <Card.Title>{survey.title}</Card.Title>
+              <Card.Description>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio excepturi fugiat repellendus ipsam, aspernatur libero nisi pariatur, eligendi iure, saepe maxime asperiores aperiam. Quis facilis voluptatum, cupiditate et incidunt sapiente.
+              </Card.Description>
+            </Card.Body>
+            <Card.Footer justifyContent='flex-end'>
+              <Button variant='outline' asChild>
+                <Link href='#'>View</Link>
+              </Button>
+            </Card.Footer>
+          </Card.Root>
+        ))}
+      </Stack>
+    </Container>
+  )
 }
